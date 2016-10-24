f30542b3f821da2f87a03f5388874b410d2a3bd7
Sukhyun Baik <neonagx@gmail.com>
2016-10-24 12:04:23 -0700

    adds restful routes for puppies

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'puppies/index'
+  root 'puppies#index'
 
-  get 'puppies/new'
-
-  get 'puppies/show'
-
-  get 'puppies/edit'
+  get 'puppies/' => 'puppies#index'
+  post 'puppies/' => 'puppies#create'
+  get 'puppies/new' => 'puppies#new', as: :new_puppy
+  get 'puppies/:id' => 'puppies#show', as: :puppy
+  get 'puppies/:id/edit' => 'puppies#edit', as: :edit_puppy
+  patch 'puppies/:id' => 'puppies#update'
+  delete 'puppies/:id' => 'puppies#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
